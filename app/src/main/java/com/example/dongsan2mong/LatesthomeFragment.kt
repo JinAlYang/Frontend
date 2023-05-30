4ed19ffb4f8bb1e76abe49216e45ea89f61bb9c2
Mingyu Choi <mg010626@naver.com>
2023-05-30 21:04:55 +0900

    Feat / 최근 본 집 화면 구현

--- app/src/main/java/com/example/dongsan2mong/LatesthomeFragment.kt
+++ app/src/main/java/com/example/dongsan2mong/LatesthomeFragment.kt
@@ -5,13 +5,53 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.dongsan2mong.databinding.FragmentLatesthomeBinding
+import com.example.dongsan2mong.databinding.RowHouseinfoBinding
 
 class LatesthomeFragment: Fragment() {
+    lateinit var binding: FragmentLatesthomeBinding
+    lateinit var adapter: HouseInfoDataAdapter
+
+    val data: ArrayList<HouseInfoData> = ArrayList()
+    val selected: ArrayList<Boolean> = ArrayList()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_latesthome, container, false)
+        binding = FragmentLatesthomeBinding.inflate(inflater, container, false)
+        initRecyclerView()
+        initData()
+        return binding.root
+    }
+
+    fun initData() {
+        data.add(HouseInfoData())
+        data.add(HouseInfoData(
+            type = "월세",
+            price = "500/74",
+            space = "33.06m^2",
+            floor = "1층",
+            area = "광진구 구의동",
+            roomNum = "투룸",
+            tempImg = 1
+        ))
+    }
+
+    fun initRecyclerView() {
+        binding.recyclerViewLatestHome.layoutManager = LinearLayoutManager(requireContext(),
+        LinearLayoutManager.VERTICAL, false)
+        adapter = HouseInfoDataAdapter(data, selected)
+        adapter.itemClickListener = object: HouseInfoDataAdapter.OnItemClickListener {
+            override fun OnItemClick(
+                data: HouseInfoData,
+                binding: RowHouseinfoBinding,
+                position: Int
+            ) {
+
+            }
+        }
+        binding.recyclerViewLatestHome.adapter = adapter
+
     }
 }
