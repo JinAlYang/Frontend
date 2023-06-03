4dd7e186eaf2bb639c9aa4c07b2fdd4578967476
Mingyu Choi <mg010626@naver.com>
2023-06-03 21:31:18 +0900

    Add [#3] FreesetData 입주가능일시데이터 삭제,  층수데이터 추가 Comment [#1] FreesetData 주석 추가

--- app/src/main/java/com/example/dongsan2mong/data/FreesetData.kt
+++ app/src/main/java/com/example/dongsan2mong/data/FreesetData.kt
@@ -5,14 +5,30 @@ import android.os.Parcel
 import android.os.Parcelable
 
 data class FreesetData(
+    // 지역 : ㅁㅁ구 ㅇㅇ동
     val region: String = "지역",
+
+    // 매매유형 : 전세, 월세, 보증금, 월세
     var typeOfSale: String = "매매유형",
+
+    // 방 갯수 : 값이 곧 방 갯수
     var numOfRooms: Int = 0,
+
+    // 편의시설 : 엘레베이터, 편의점, 세탁소, 대형마트 등
     var amenities: String = "편의시설",
+
+    // 건물 유형 : 원룸, 투룸, 오피스텔 중 하나
     var buildingType: String = "건물유형",
+
+    // 평형 :
     var roomArea: Int = 0,
-    var availableDate: String = "입주가능일시",
+
+    // 층수 : 반지하, 지상층(구체적으로 몇층인지도?), 옥탑
+    var roomFloor: String = "지상층",
+
+    // 싱크대, 에어컨, 세탁기, 냉장고 등
     var options: String = "옵션",
+
     var freesetTitle: String = "프리셋 제목"
 ) : Parcelable {
     constructor(parcel: Parcel) : this(
@@ -34,7 +50,7 @@ data class FreesetData(
         parcel.writeString(amenities)
         parcel.writeString(buildingType)
         parcel.writeInt(roomArea)
-        parcel.writeString(availableDate)
+        parcel.writeString(roomFloor)
         parcel.writeString(options)
         parcel.writeString(freesetTitle)
     }
