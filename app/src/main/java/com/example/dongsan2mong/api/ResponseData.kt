37206601dd8637540b32169bef4e313bf90e913b
Mingyu Choi <mg010626@naver.com>
2023-06-04 04:58:41 +0900

    Add [#1] Retrofit 관련 코드 추가

---
+++ app/src/main/java/com/example/dongsan2mong/api/ResponseData.kt
@@ -0,0 +1,7 @@
+package com.example.dongsan2mong.api
+
+data class ResponseData(
+    val temp: Int  = 0
+) {
+
+}
---
+++ app/src/main/java/com/example/dongsan2mong/api/RetrofitService.kt
@@ -0,0 +1,17 @@
+package com.example.dongsan2mong.api
+
+import android.provider.ContactsContract.Data
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.FieldMap
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+
+interface RetrofitService {
+
+}
