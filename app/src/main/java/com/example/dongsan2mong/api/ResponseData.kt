3eb5df1bd1c4094d860a7ce08084dfafdcfad623
Mingyu0626 <mg010626@naver.com>
2023-06-04 18:56:32 +0900

    Feat [#16] 백에서 받아오는 데이터 클래스 추가, RetroitService 추가

--- app/src/main/java/com/example/dongsan2mong/api/ResponseData.kt
+++ app/src/main/java/com/example/dongsan2mong/api/ResponseData.kt
@@ -1,7 +1,74 @@
 package com.example.dongsan2mong.api
 
 data class ResponseData(
-    val temp: Int  = 0
-) {
+    val id: Int,
+    val name: String,
+    val email: String,
+    val gender: String,
+    val age: Int
+)
 
-}
+data class memberInfo(
+    val id: Int,
+    val name: String,
+    val email: String,
+    val gender: String,
+    val age: Int,
+    val livingExpenses: Int,
+    val savingMoney: Int,
+    val loanInterest: Int
+)
+
+data class wishListInfo(
+    val id: Int,
+    val name: String,
+    val email: String,
+    val gender: String,
+    val age: Int
+)
+
+data class realEstate(
+    val id: Int,
+    val name: String,
+    val email: String,
+    val gender: String,
+    val age: Int
+)
+
+data class realEstateDetail(
+    val realEstateDetailId: Int,
+    val realEstateId: Int,
+    val managementFees: Int,
+
+    // 집 옵션
+    val sinkStatus: Boolean,
+    val airConditionStatus: Boolean,
+    val showBoxStatus: Boolean,
+    val washingMachineStatus: Boolean,
+    val refrigeratorStatus: Boolean,
+    val closetStatus: Boolean,
+    val gasStovesStatus: Boolean,
+    val bedStatus: Boolean,
+
+    // 주변 생활시설
+    val cafeStatus: Boolean,
+    val convenienceStoreStatus: Boolean,
+    val laundryStatus: Boolean,
+    val supermarketStatus: Boolean,
+    val busStationStatus: Boolean,
+    val subwayStatus: Boolean,
+
+    val detailInfo: String,
+)
+
+data class presetResponse(
+    val preset: List<presetInfo>
+)
+
+data class presetInfo(
+    val id: Int,
+    val name: String,
+    val email: String,
+    val gender: String,
+    val age: Int
+)
--- app/src/main/java/com/example/dongsan2mong/api/RetrofitService.kt
+++ app/src/main/java/com/example/dongsan2mong/api/RetrofitService.kt
@@ -1,17 +1,67 @@
 package com.example.dongsan2mong.api
 
-import android.provider.ContactsContract.Data
-import okhttp3.ResponseBody
 import retrofit2.Call
-import retrofit2.http.Body
-import retrofit2.http.FieldMap
-import retrofit2.http.FormUrlEncoded
-import retrofit2.http.GET
-import retrofit2.http.POST
-import retrofit2.http.Path
-import retrofit2.http.Query
+import retrofit2.http.*
 
 
 interface RetrofitService {
 
+    //Member
+    @GET("member/{memberId}")
+    fun getMemberInfo(@Path("memberId") number: Int): Call<memberInfo>
+
+    @POST("member")
+    fun addMemberInfo()
+
+    @PUT("member/{memberId}")
+    fun modifyMemberInfo()
+
+
+    //Region
+
+
+    //WishList
+    @GET("wishList/recentHome/{memberId}")
+    fun getRecentHomeList(@Path("memberId") number: Int): Call<ResponseData>
+
+    @GET("wishList/zzimHome/{memberId}")
+    fun getZzimHomeList(@Path("memberId") number: Int): Call<ResponseData>
+
+
+    //RealEstateDetail
+    @GET("RealEstateDetail/{RealEstate_id}")
+    fun getRealEstateDetail(@Path("RealEstate_id") number: Int): Call<ResponseData>
+
+
+    //RealEstate
+    @GET("RealEstate/{RealEstate_id}")
+    fun getRealEstate(@Path("RealEstate_id") number: Int): Call<ResponseData>
+
+
+    //SearchPreset
+    @GET("preset/find/{member_id}")
+    fun getPresetList(@Path("member_id") number: Int): Call<ResponseData>
+
+    @GET("preset/find/{member_id}/{preset_id}")
+    fun getPreset(
+        @Path("member_id") number: Int,
+        @Path("preset_id") presetId: Int
+    ): Call<ResponseData>
+
+
+    //Map
+
+
+    // Example
+    /*@POST("user/register")
+    fun postUser(@Body params: HashMap<String, String>): Call<UserInfo>
+
+    @GET("user/initNang/{userNo}")
+    fun getNang(@Path("userNo") number: Int): Call<ResponseBody>
+
+    @GET("dialog/open/{userNo}")
+    fun getOpening(@Path("userNo") number: Int): Call<ResponseBody>
+
+    @GET("dialog/isCreate/{userNo}")
+    fun getOK(@Path("userNo") number: Int): Call<ResponseBody>*/
 }
