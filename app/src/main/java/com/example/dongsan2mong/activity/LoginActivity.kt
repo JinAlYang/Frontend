3beb95b0c73bba7711c75a167d8e1b97af826bda
blueme0 <ksytpdus0304@gmail.com>
2023-06-11 15:40:35 +0900

    Feat [#22] MapFragment - 카카오 로그인 버튼 왼쪽 : 회원가입, 오른쪽 : 로그인.. 수정...

--- app/src/main/java/com/example/dongsan2mong/activity/LoginActivity.kt
+++ app/src/main/java/com/example/dongsan2mong/activity/LoginActivity.kt
@@ -73,13 +73,10 @@ class LoginActivity : AppCompatActivity() {
 
                                 i.putExtra("memberInfo", memberInfo)
                                 startActivity(i)
-                                finish()
+//                                finish()
 //                                    nickname.text = "닉네임: ${user?.kakaoAccount?.profile?.nickname}"
                             }
                         }
-//                        getLoginInfoHttp(0)
-
-
                     }
                 }
                 // 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
@@ -112,6 +109,75 @@ class LoginActivity : AppCompatActivity() {
                             }
                             val intent = Intent(this@LoginActivity, MainActivity::class.java)
                             startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
+//                            finish()
+                        }
+                    }
+                } else {
+                    UserApiClient.instance.loginWithKakaoAccount(
+                        this@LoginActivity,
+                        callback = callback
+                    )
+                }
+            }
+
+            signupKakaoBtn.setOnClickListener {
+                val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+                    if (error != null) {
+                        Log.e("LOGIN", "카카오계정으로 로그인 실패", error)
+
+                    } else if (token != null) {
+                        Log.i("LOGIN", "카카오계정으로 로그인 성공 ${token.accessToken}")
+                        UserApiClient.instance.accessTokenInfo { tokenInfo, error ->
+                            UserApiClient.instance.me { user, error ->
+                                Log.d(
+                                    "kakaologin",
+                                    "name : ${user?.kakaoAccount?.profile?.nickname}, 이메일 : ${user?.kakaoAccount?.email}"
+                                )
+                                memberInfo = MemberInfoData(
+                                    20010304,
+                                    user?.kakaoAccount?.profile?.nickname.toString(),
+                                    user?.kakaoAccount?.email.toString(),
+                                )
+                                val i = Intent(this@LoginActivity, SignupActivity::class.java)
+
+                                i.putExtra("memberInfo", memberInfo)
+                                startActivity(i)
+//                                finish()
+//                                    nickname.text = "닉네임: ${user?.kakaoAccount?.profile?.nickname}"
+                            }
+                        }
+                    }
+                }
+                // 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
+                if (UserApiClient.instance.isKakaoTalkLoginAvailable(this@LoginActivity)) {
+                    UserApiClient.instance.loginWithKakaoTalk(this@LoginActivity) { token, error ->
+                        if (error != null) {
+                            Log.e("LOGIN", "카카오톡으로 로그인 실패", error)
+
+                            // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
+                            // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
+                            if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+                                return@loginWithKakaoTalk
+                            }
+
+                            // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인 시도
+                            UserApiClient.instance.loginWithKakaoAccount(
+                                this@LoginActivity,
+                                callback = callback
+                            )
+                        } else if (token != null) {
+                            Log.i("LOGIN", "카카오톡으로 로그인 성공 ${token.accessToken}")
+                            UserApiClient.instance.accessTokenInfo { tokenInfo, error ->
+                                UserApiClient.instance.me { user, error ->
+                                    Log.d(
+                                        "kakaologin",
+                                        "name : ${user?.kakaoAccount?.profile?.nickname}, 이메일 : ${user?.kakaoAccount?.email}"
+                                    )
+//                                    nickname.text = "닉네임: ${user?.kakaoAccount?.profile?.nickname}"
+                                }
+                            }
+                            val intent = Intent(this@LoginActivity, SignupActivity::class.java)
+                            startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
                             finish()
                         }
                     }
@@ -121,11 +187,8 @@ class LoginActivity : AppCompatActivity() {
                         callback = callback
                     )
                 }
-
-//                loginIntent.putExtra("isKakao", true)
-//                launcher.launch(loginIntent)
-
             }
+
             loginNaverBtn.setOnClickListener {
                 loginIntent.putExtra("isKakao", false)
                 launcher.launch(loginIntent)
